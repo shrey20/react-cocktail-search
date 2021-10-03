553cd90cb15a253c3edb14aa36212359d28900b5
shreyans <shreyans@shreyanss-MacBook-Air.local>
2021-10-03 17:11:32 -0500

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Earum dolorum blanditiis veniam maiores ex. 
+        Deserunt perspiciatis ab quod exercitationem dicta incidunt iure quasi cupiditate, 
+        similique necessitatibus voluptatem voluptate, at ullam? 
+        Velit quia facere ipsum dicta id illum cumque odit laudantium!
+      </p>
+    </section>
+  )
+}
+
+export default About
